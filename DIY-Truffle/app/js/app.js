230f8c34ad14a55f72c3544176b8238f00976fa6
jyu520 <jyu520@stanford.edu>
2018-05-29 03:04:19 -0700

    added deposit and withdraw functionality

--- DIY-Truffle/app/js/app.js
+++ DIY-Truffle/app/js/app.js
@@ -2,9 +2,12 @@ function app() {
     if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
     web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
     console.log("Using web3 version: " + Web3.version);
-  
-    var contract;
-    var userAccount;
+	
+	var contract;
+	var contractAddress;
+
+	var userAccount;
+	var divisor = 1000000000000000000;
   
     var contractDataPromise = $.getJSON('DIY.json');
     var networkIdPromise = web3.eth.net.getId(); // resolves on the current network id
@@ -15,14 +18,15 @@ function app() {
         var contractData = results[0];
         var networkId = results[1];
         var accounts = results[2];
-        userAccount = accounts[0];
+		userAccount = accounts[0];
+		console.log(userAccount)
   
 		// (todo) Make sure the contract is deployed on the network to which our provider is connected
 		if (!(networkId in contractData.networks)) {
 			throw new Error("Contract not found in selected Ethereum network on MetaMask.");
 		}
   
-        var contractAddress = contractData.networks[networkId].address;
+        contractAddress = contractData.networks[networkId].address;
         contract = new web3.eth.Contract(contractData.abi, contractAddress);
       })
       .then(refreshBalance)
@@ -30,10 +34,20 @@ function app() {
 
     function refreshBalance() { // Returns web3's PromiEvent
 		// Calling the contract (try with/without declaring view)
-		contract.methods.balanceOf(userAccount).call().then(function (balance) {
-		$('#display').text(balance + " DIY");
+		contract.methods.ethBalanceOf(userAccount).call().then(function (balance) {
+		balance = (balance / divisor).toFixed(2)
+		console.log(userAccount, balance)
+		$('#display').text(balance + " ETH");
+		});
+		
+		contract.methods.ethBalanceOf(contractAddress).call().then(function (balance) {
+		balance = (balance / divisor).toFixed(2)
+		console.log(contractAddress, balance)
+		$('#ethDisplay').text("Your Index Balance: " + balance + " ETH");
 		$("#loader").hide();
 		});
+
+
 	}
 	
 	function transfer(to, amount) {
@@ -49,6 +63,69 @@ function app() {
 			});
 	}
 
+	function deposit(amount){
+		console.log("depositing " + amount + " eth to smart contract")
+		// var etherAmount = web3.utils.toBN(amount);
+		var weiValue = web3.utils.toWei(amount,'ether');
+		contract.methods.deposit().send({from: userAccount, value:weiValue})
+		.then(refreshBalance)
+		.catch(function (e) {
+			$("#loader").hide();
+			web3.eth.getBalance(contractAddress, (err, balance) => {
+			balance = web3.utils.fromWei(balance, "ether") + " ETH"
+			console.log(balance)
+			})
+		});
+		
+		// .then(contract.methods.approveCall(contractAddress, weiValue).send({from: userAccount}))
+		
+		// web3.eth.sendTransaction({
+		// 	from: userAccount,
+		// 	to: contractAddress,
+		// 	value: weiValue
+		// })
+	}
+	
+
+	function withdraw(amount){
+		console.log("withdrawing " + amount + " eth to metamask")
+		// var etherAmount = web3.utils.toBN(amount);
+		var weiValue = web3.utils.toWei(amount,'ether');
+		
+		// web3.eth.getBalance(contractAddress, (err, balance) => {
+		// balance = web3.utils.fromWei(balance, "ether") + " ETH"
+		// console.log(balance)
+		// })
+
+		contract.methods.withdraw(weiValue).send({from: userAccount})
+		.then(refreshBalance)
+		.catch(function (e) {
+			$("#loader").hide();
+			web3.eth.getBalance(contractAddress, (err, balance) => {
+			balance = web3.utils.fromWei(balance, "ether") + " ETH"
+			console.log(balance)
+			})
+		});
+
+		// web3.eth.sendTransaction({
+		// 	from: contractAddress,
+		// 	to: userAccount,
+		// 	value: weiValue
+		// })
+		
+		// contract.methods.withdraw(userAccount)
+		// .send({from: contractAddress, to: userAccount, value: weiValue})
+	}
+
+	// /// Withdraw a bid that was overbid.
+    // function withdraw(amount) public returns (bool success) {
+	// 	if (!msg.sender.send(amount)) {
+	// 		return false;
+	// 	}
+    //     return true;
+    // }
+
+
 	function mint(amount) {
 		console.log(amount)
 		if (!amount) return console.log("Fill in amount field");
@@ -60,8 +137,35 @@ function app() {
 			.catch(function (e) {
 			$("#loader").hide();
 			});
-	}
+	}    
 	
+	$("#deposit").click(function(){
+		let amount = $("#eth_value").val()
+		deposit(amount);
+	})
+
+	$("#withdraw").click(function(){
+		let amount = $("#eth_value").val()
+		withdraw(amount);
+	})
+
+	$("#checkBalance").click(function(){
+		web3.eth.getBalance(userAccount, function(err, balance){
+			if (err) {
+				console.log(`getBalance error: ${err}`);
+			} else {
+				console.log(`Balance [${userAccount}]: ${web3.utils.fromWei(balance, "ether")}`);
+			}
+		});
+		web3.eth.getBalance(contractAddress, function(err, balance2){
+			if (err) {
+				console.log(`getBalance error: ${err}`);
+			} else {
+				console.log(`Balance [${contractAddress}]: ${web3.utils.fromWei(balance2, "ether")}`);
+			}
+		});
+	})
+
 	$("#button").click(function() {
 		var toAddress = $("#address").val();
 		var amount = $("#amount").val();
